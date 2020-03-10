d6746c8351e7c591237c8a1f6343ea4bc6fb7703
chikechris <cchike2017@gmail.com>
2020-03-10 02:57:37 -0400

    insert and append function added

--- src/dynamic_array.py
+++ src/dynamic_array.py
@@ -4,3 +4,21 @@ class DynamicArray:
         # How much total space is currently allocated wether we are using it or not
         self.capacity = capacity
         self.storage = [None] * self.capacity
+
+    def insert(self, index, value):
+        if self.count == self.capacity:
+            #TODO: resize
+            print("Error, array is full")
+            return
+
+        #Shift everything to the right"
+            #starting, stopping
+        for i in range(self.count, index, -1):
+            self.storage[i] = self.storage[i - 1]
+
+        #Insert our value:
+        self.storage[index] = value
+        self.count += 1
+
+    def append(self, value):
+        self.insert(self.count, value)
